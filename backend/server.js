87204d001b7bbdca67b40ca2c048b4d4a3fd3bae
hari <thisismekrishnan@gmail.com>
2023-07-23 00:33:25 +0530

    for hosting

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import exp from "constants";
+dotenv.config();
+const port = process.env.port || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Hello World"));
+}
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
